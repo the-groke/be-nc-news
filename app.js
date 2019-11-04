d83ffabff9fb6f113a1ce1397f4d95403c5cba53
Benjamin Rutledge <benjaminrutledge@Benjamins-MacBook-Pro-2.local>
2019-11-04 15:56:24 +0000

    installed and required cors

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const apiRouter = require("./routes/api-router");
+const apiRouter = require('./routes/api-router');
 const {
   handle400s,
   handle404s,
   handle405s,
   handle422s,
   handle500s
-} = require("./errors");
+} = require('./errors');
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 // wildcard
-app.all("/*", (req, res, next) => {
-  res.status(404).send({ msg: "not found" });
+app.all('/*', (req, res, next) => {
+  res.status(404).send({ msg: 'not found' });
 });
 
 // error-handling middleware functions
